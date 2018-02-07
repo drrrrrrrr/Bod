8ff3ab833e3f7b8910b85c6ae6b689ce491cf6bc
Dima <rudoydmitry@yandex.ru>
2018-02-08 00:53:48 +0300

    добавлены сайты

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bod.Startup))]
+namespace Bod
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
